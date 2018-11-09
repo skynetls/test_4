f6736af30caf74b94307f5a09d2bfe5c500dd961
skynetls <lyudocheksi@gmail.com>
2018-11-10 02:32:00 +0300

    Initial commit

---
+++ src/main/java/task_10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class task_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String s = scanner.nextLine();
+
+        System.out.println(s);
+    }
+}
