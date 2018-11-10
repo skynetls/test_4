68aca34714876c569bff5dae6b2a3ec9441129fa
skynetls <lyudocheksi@gmail.com>
2018-11-10 03:54:38 +0300

    task_12

---
+++ src/main/java/task_12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class task_12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String s1 = scanner.nextLine();
+        System.out.println("Введите строку: ");
+        String s2 = scanner.nextLine();
+
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+        if (n1 > n2){
+            System.out.println("Строка с наибольшей длинной: "+s1);
+        }
+        else {
+            System.out.println("Строка с наибольшей длинной: "+s2);
+        }
+    }
+}
